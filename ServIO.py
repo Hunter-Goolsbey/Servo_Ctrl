805761c46c271f06836c30381b5243436207c175
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-08-12 19:23:21 -0700

    Create ServIO.py

---
+++ ServIO.py
@@ -0,0 +1,114 @@
+from inputs import get_gamepad
+import math
+import threading
+from adafruit_servokit import ServoKit
+
+class XboxController(object):
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
+
+    def __init__(self):
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+        self.LeftTrigger = 0
+        self.RightTrigger = 0
+        self.LeftBumper = 0
+        self.RightBumper = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LeftThumb = 0
+        self.RightThumb = 0
+        self.Back = 0
+        self.Start = 0
+        self.LeftDPad = 0
+        self.RightDPad = 0
+        self.UpDPad = 0
+        self.DownDPad = 0
+
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+
+
+    def read(self): # return the buttons/triggers that you care about in this method
+        x = self.LeftJoystickX
+        y = self.LeftJoystickY
+        a = self.A
+        bbutton = self.B
+        ybutton = self.Y
+        lt = self.LeftTrigger
+        rt = self.RightTrigger
+        b = self.X # b=1, x=2
+        rb = self.RightBumper
+        return [x, y, bbutton, ybutton, a, b, rb, lt, rt]
+
+
+    def _monitor_controller(self):
+        while True:
+            events = get_gamepad()
+            for event in events:
+                if event.code == 'ABS_Y':
+                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_X':
+                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RY':
+                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RX':
+                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_Z':
+                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'ABS_RZ':
+                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'BTN_TL':
+                    self.LeftBumper = event.state
+                elif event.code == 'BTN_TR':
+                    self.RightBumper = event.state
+                elif event.code == 'BTN_SOUTH':
+                    self.A = event.state
+                elif event.code == 'BTN_NORTH':
+                    self.X = event.state
+                elif event.code == 'BTN_WEST':
+                    self.Y = event.state
+                elif event.code == 'BTN_EAST':
+                    self.B = event.state
+                elif event.code == 'BTN_THUMBL':
+                    self.LeftThumb = event.state
+                elif event.code == 'BTN_THUMBR':
+                    self.RightThumb = event.state
+                elif event.code == 'BTN_SELECT':
+                    self.Back = event.state
+                elif event.code == 'BTN_START':
+                    self.Start = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY1':
+                    self.LeftDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY2':
+                    self.RightDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY3':
+                    self.UpDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY4':
+                    self.DownDPad = event.state
+if __name__ == '__main__':
+    joy = XboxController()
+    kit = ServoKit(channels=16)
+    kit.continuous_servo[0].throttle = 0
+    while True:
+        print (joy.read()[7])
+        if joy.read()[7]  > 0.01:
+            kit.continuous_servo[0].throttle = joy.read()[7]/4
+        elif joy.read()[8] > 0.01:
+            kit.continuous_servo[0].throttle = -1*(joy.read()[8]/4)
+        else:
+            kit.continuous_servo[0].throttle = 0
+#        if joy.read()[4] != 0:
+#            kit.continuous_servo[0].throttle = -1
+        if joy.read()[5] != 0:
+            while joy.read()[5] != 0:
+                kit.continuous_servo[0].throttle = 0
+#        elif joy.read()[2] != 0:
+#            kit.continuous_servo[0].throttle = 0
+#        elif joy.read()[3] != 0:
+#            kit.continuous_servo[0].throttle = 1]
